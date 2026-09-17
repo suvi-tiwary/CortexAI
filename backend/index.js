9fa2cabf7a2ccd71c695f0ff3180caca43693041
Suvi Tiwary <suvikumari345@gmail.com>
2026-09-18 01:20:25 +0530

    1

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 
